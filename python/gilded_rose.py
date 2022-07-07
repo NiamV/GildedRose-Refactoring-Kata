19ec9b7066de27ac7b79a48e3dc1777bdef18ba5
Niam Vaishnav <niam.vaishnav@softwire.com>
2022-07-07 11:45:55 +0100

    Refactored backstage pass quality changing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,6 +5,15 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def increase_backstage_quality(self, item):
+        if item.sell_in < 6 and item.quality < 48:
+                item.quality += 3
+        elif item.sell_in < 11 and item.quality < 49:
+                item.quality += 2
+        elif item.quality < 50:
+            item.quality += 1
+
+
     def update_quality(self):
         improving_items = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]
         constant_items = ["Sulfuras, Hand of Ragnaros"]
@@ -13,15 +22,11 @@ class GildedRose(object):
             if (not item.name in improving_items + constant_items) and item.quality > 0:
                 item.quality = item.quality - 1
             elif not item.name in constant_items:
-                if item.quality < 50:
+                if item.quality < 50 and item.name != "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                else:
+                    self.increase_backstage_quality(item)
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
