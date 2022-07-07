d63153c70ca71d7a437607caed95af0331a03f78
Niam Vaishnav <niam.vaishnav@softwire.com>
2022-07-07 11:14:56 +0100

    Added Unit Tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -9,7 +9,70 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+
+    def test_quality_degrades_double(self):
+        items = [Item("foo", 1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        currentQual = items[0].quality
+        gilded_rose.update_quality()
+        self.assertEqual((10 - currentQual)*2, currentQual - items[0].quality)
+
+    def test_nonnegative_quality(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        for _ in range(0, 10):
+            gilded_rose.update_quality()
+        self.assertTrue(items[0].quality >= 0)
+
+    def test_agedbrie_improves(self):
+        items = [Item("Aged Brie", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(items[0].quality > 0)
+
+    def test_not_more_than_50_quality(self):
+        items = [Item("Aged Brie", 0, 49)]
+        gilded_rose = GildedRose(items)
+        for _ in range(0, 10):
+            gilded_rose.update_quality()
+        self.assertTrue(items[0].quality <= 50)
+
+    def test_Sulfuras_is_constant(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 6)
+        self.assertEqual(items[0].sell_in, 5)
+
+    def test_Backstage_Passes_Aged_Correctly_MoreThan10(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 11, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 7)
+
+    def test_Backstage_Passes_Aged_Correctly_LessThan10(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 8)
+
+    def test_Backstage_Passes_Aged_Correctly_LessThan5(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 9)
+
+    def test_Backstage_Passes_Aged_Correctly_LessThan0(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 0)
+
+
+
+
 
         
 if __name__ == '__main__':
